e17696deee617ba77f485983e48b46c3f31ef8cc
dalewang1995 <dalewang1995@gmail.com>
2019-10-23 10:59:57 +0800

    init ts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,30 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{message}}</h2>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '../components/HelloWorld.vue'
+
+@Component({
+  components: { HelloWorld }
+})
+
+export default class Home extends Vue {
+  message: string = 'Hello! dale'
+
+  // 组件方法也可以直接声明为实例的方法
+  onClick (): void {
+    window.alert(this.message)
   }
 }
+
 </script>
+
+<style lang="scss">
+</style>
